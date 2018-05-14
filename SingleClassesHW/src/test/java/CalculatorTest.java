0071342097d6b4bdbf2dc0e245b4644bd61a3351
sarahmurphy86 <sarah.graham86@gmail.com>
2018-05-14 16:29:51 +0100

    calculator tests passed

--- SingleClassesHW/src/test/java/CalculatorTest.java
+++ SingleClassesHW/src/test/java/CalculatorTest.java
@@ -1,2 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+    Calculator calculate;
+
+
+    @Before
+    public void before(){
+        calculate = new Calculator(10, 5);
+    }
+
+    @Test
+    public void add(){
+        assertEquals(15, calculate.add());
+    }
+
+    @Test
+    public void subtract(){
+        assertEquals(5, calculate.subtract());
+    }
+
+    @Test
+    public void multiply(){
+        assertEquals(50, calculate.multiply());
+    }
+
+    @Test
+    public void divide(){
+        assertEquals(2, calculate.divide());
+    }
 }
